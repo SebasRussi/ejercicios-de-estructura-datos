7c648bdf2cb1e808a57c979ce34708899086b9c7
CodeDasg <dsanabriagomez@outlook.es>
2023-08-17 13:31:19 -0500

    ejercicios 2 y 3

---
+++ ejercicios/__init__.py
@@ -0,0 +1,6 @@
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+suma = 0
+for i in numeros:
+    suma = suma + i
+    prom = suma/i
+print("la suma de los elementos de la lista es:", suma, prom)
--- ejercicios/ejercicios 1-4.py
+++ ejercicios/ejercicios 1-4.py
@@ -1,8 +1,20 @@
 class Student:
-    def __init__(self,name, age, grades):
+    def __init__(self, name, age):
         self.name = name
         self.age = age
-        self.grades = [int]
+        self.grades: list = [2, 3, 4, 5, 4]
 
-    def add_grade(self):
-        pass
+    def add_grade(self, numero):
+        self.grades.append(numero)
+
+    def average_grade(self):
+        for i in self.grades:
+            suma = suma + i
+            average = suma/i
+
+
+
+if __name__ == "__main__":
+    est1 = Student("jorge", 14)
+    est1.add_grade(5)
+    print(est1)
