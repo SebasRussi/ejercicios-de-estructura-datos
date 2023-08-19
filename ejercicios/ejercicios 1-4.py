b567fd93c97e498ccec5ec8b63cedece2e3ccc29
sebas <srr1767@gmail.com>
2023-08-18 21:37:00 -0500

    ejercicio 1-4 arreglados

--- ejercicios/ejercicios 1-4.py
+++ ejercicios/ejercicios 1-4.py
@@ -1,20 +1,26 @@
 class Student:
-    def __init__(self, name, age):
+    def __init__(self, name, age, grades):
         self.name = name
         self.age = age
-        self.grades: list = [2, 3, 4, 5, 4]
+        self.grades: list = grades
 
     def add_grade(self, numero):
         self.grades.append(numero)
 
     def average_grade(self):
-        for i in self.grades:
-            suma = suma + i
-            average = suma/i
-
+        if len(self.grades) == 0:
+            return 0
+        total = sum(self.grades)
+        average = total / len(self.grades)
+        return average
 
 
 if __name__ == "__main__":
-    est1 = Student("jorge", 14)
-    est1.add_grade(5)
-    print(est1)
+    student1 = Student("Alice", 18, [85, 92, 78, 90])
+    student2 = Student("Bob", 19, [70, 65, 80, 75])
+
+    print(f"Name of student1: {student1.name}")
+    print(f"Age of student1: {student1.age}")
+    print(f"Grades of student1: {student1.grades}")
+    student1.add_grade(88)
+    print(f"Grades of student1 after adding: {student1.grades}")
